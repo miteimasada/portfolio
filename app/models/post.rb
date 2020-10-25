51699d2a9e2d1f7cbbf45ccaeaead7be3537da69
shin <miteimasada@gmail.com>
2020-10-25 14:13:22 +0000

    コメント投稿のビューを修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   has_rich_text :content
   validates :content, {presence: true}
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
