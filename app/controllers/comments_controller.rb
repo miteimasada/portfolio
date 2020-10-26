626d0f48c3c229755409c3588edc19229def0c81
shin <miteimasada@gmail.com>
2020-10-26 16:01:47 +0000

    ユーザー詳細の表示修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,10 +6,22 @@ class CommentsController < ApplicationController
     @comment = post.comments.build(comment_params)
     @comment.user_id = @current_user.id
     if @comment.save
-      flash[:success] = "コメントしました"
+      flash[:notice] = "コメントしました"
       redirect_to("/posts/#{params[:post_id]}")
     else
-      flash[:success] = "コメントできませんでした"
+      flash[:notice] = "コメントできませんでした"
+      redirect_to("/posts/#{params[:post_id]}")
+    end
+  end
+
+  def destroy
+    @comment=Comment.find(params[:id])
+    @comment.destroy
+    if @comment.destroy
+      flash[:notice] = "コメントを削除しました"
+      redirect_to("/posts/#{params[:post_id]}")
+    else
+      flash[:notice] = "コメントの削除に失敗しました"
       redirect_to("/posts/#{params[:post_id]}")
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -32,4 +32,5 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: :create
   end
+  resources :comments, only: :destroy
 end
