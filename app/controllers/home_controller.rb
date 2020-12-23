1f63abaf01c45275e346896adb44919908a8eb25
miteimasada <miteimasada@gmail.com>
2020-12-23 12:46:23 +0000

    ページネーションの追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def top
-    @posts = Post.all.order(created_at: :desc)
+    @posts = Post.page(params[:page]).per(5)
     @user = User.find_by(id: params[:id])
   end
 end
