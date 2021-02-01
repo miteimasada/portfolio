688c473c30f290aa6f43575cb6fb311fe6d5ef3a
miteimasada <miteimasada@gmail.com>
2021-02-01 17:57:19 +0900

    UIの改善

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,22 @@
 class HomeController < ApplicationController
+
   def top
-    @posts = Post.page(params[:page]).order(created_at: :desc).per(10)
-    @user = User.find_by(id: params[:id])
-    @popular = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(5).pluck(:post_id))
+    @posts = Post.all.page(params[:page]).order(created_at: :desc).per(10)
+    @popular = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(10).pluck(:post_id))
   end
+
+  def index
+    @user = User.find_by(id: params[:id])
+    @posts = Post.all.page(params[:page]).order(created_at: :desc).per(10)
+    @popular = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(10).pluck(:post_id))
+  end
+
+  def timeline
+    @timeline = Post.where(user_id: [@current_user.following]).order(created_at: :desc)
+  end
+
+  def likes
+    @likes = Like.where(user_id: @current_user.id)
+  end
+
 end
