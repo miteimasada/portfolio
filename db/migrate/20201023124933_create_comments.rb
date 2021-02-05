6cca1477cf830b0ad358ed9b3f5e72a97812a7db
miteimasada <miteimasada@gmail.com>
2021-02-05 22:21:28 +0900

    migrate/commentsの修正

--- db/migrate/20201023124933_create_comments.rb
+++ db/migrate/20201023124933_create_comments.rb
@@ -1,6 +1,5 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
-    drop_table :comments
     create_table :comments do |t|
       t.string :content
       t.references :user, null: false, foreign_key: true
